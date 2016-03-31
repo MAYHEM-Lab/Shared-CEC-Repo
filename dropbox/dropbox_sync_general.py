096f7e7bc9652f4d84dd1925b9e64b157ed1a531
ckrintz <ckrintz@yahoo.com>
2016-03-30 19:57:41 -0700

    minor testing and checking added

--- dropbox/dropbox_sync_general.py
+++ dropbox/dropbox_sync_general.py
@@ -7,19 +7,25 @@ def process_files(fname,fnamenew,fnameout,err_fname='errors.txt',dont_append=Fal
     ''' Calculates diff between fname and fnamenew and appends the change to fname.
         fnameout is temp file used to store the diff.
     '''    
-    os.system("diff {0} {1}| grep '^>'|awk -F'> ' '{{print $2}}' > {2}".format(fname,fnamenew,fnameout))
+    cmd = "diff '{0}' '{1}'| grep '^>'|awk -F'> ' '{{print $2}}' > {2}".format(fname,fnamenew,fnameout)
     if DEBUG:
-        res = os.popen("cat {0}".format(fnameout)).read()
+        print 'command: {0}'.format(cmd)
+
+    os.system(cmd)
+    
+    if DEBUG:
+        res = os.popen("cat '{0}'".format(fnameout)).read()
         print 'DEBUG: adding diffs: '
         print res
+
 
     #append fnameout to fname
     if dont_append:
         print 'testing: not appending to file {0}'.format(fname)
     else:
-        os.system("cat {0} >> {1}".format(fnameout,fname))
+        os.system("cat '{0}' >> '{1}'".format(fnameout,fname))
         #sanity check - there should be nothing in fnameout file
-        os.system("diff {0} {1} |grep '^>'|awk -F'> ' '{{print $2}}' > {2}".format(fname,fnamenew,fnameout))
+        os.system("diff '{0}' '{1}' |grep '^>'|awk -F'> ' '{{print $2}}' > '{2}'".format(fname,fnamenew,fnameout))
         if os.path.getsize("{0}".format(fnameout)) != 0:
             err_fname = _ERRORS + "/" + fnamenew.split("/")[-1:] + ".err"
             errstr =  'ERROR: difference between {0} and {1} should be 0, it is {2}'.format(fname,fnamenew, os.path.getsize("{0}".format(fnameout)))
@@ -41,20 +47,29 @@ def main():
     if len(sys.argv) < 3:
         usage()
     
-    DROPBOX_DIR = sys.argv[1].strip("/") 
-    DATA_DIR = sys.argv[2].strip("/")
+    DROPBOX_DIR = sys.argv[1]
+    DATA_DIR = sys.argv[2]
+    print '''Monitoring {0}'''.format(DROPBOX_DIR)
+    print '''Using {0} for data'''.format(DATA_DIR)
     
     file_list = os.listdir(DROPBOX_DIR)
     for files in file_list:
+        print '''Processing file {0}'''.format(files)
         data_file = DATA_DIR + "/" + files
+        print '''Processing file {0}'''.format(data_file)
         abs_files = DROPBOX_DIR + "/" + files
         if os.path.isfile(data_file):
             process_files(data_file, abs_files, files + '.tmp')
         else:
-            pass
             #write to errors directory in Dropbox.
+            pass
 
 if __name__ == "__main__":
+    '''Setup:  the same csv file should be in DropBox folder and in DATA_DIR.
+       Then this program should be called in a sleep loop to check when/if there
+       is a difference between these two files, where there is a difference
+       append it to the file in the DATA_DIR. Errors are written to CWD in errors.txt
+    '''
     main()
 
 
